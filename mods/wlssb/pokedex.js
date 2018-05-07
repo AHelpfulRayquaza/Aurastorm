6773b3a399ce05206ac925960a0fba84ee052a3a
A Flying Phantom <35690029+PowerHitter0418@users.noreply.github.com>
2018-05-07 12:01:06 +0530

    Create pokedex.js

---
+++ mods/wlssb/pokedex.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.BattlePokedex = {
+	darkrai: {
+		inherit: true,
+		baseStats: {hp: 60, atk: 105, def: 60, spa: 120, spd: 60, spe: 105},
+	},
+	totodile: {
+		inherit: true,
+		baseStats: {hp: 65, atk: 80, def: 80, spa: 59, spd: 63, spe: 58},
+	},
+};
+© 2018 GitHub, Inc.
